21f31546e748d45a7bde223943b92c7af58d827f
Joel Burget <joelburget@gmail.com>
2016-07-21 08:36:55 -0400

    Initial commit.

---
+++ agent.js
@@ -0,0 +1,40 @@
+var env = {
+  getNumStates: function() { return 7 * 5 + 1; },
+  getMaxNumActions: function() { return 4; },
+};
+
+var spec = {
+  num_hidden_units: 200,
+  experience_add_every: 2,
+  learning_steps_per_iteration: 10,
+  experience_size: 20000,
+  alpha: 0.01,
+  epsilon: 1.0,
+  gamma: 0.99 // minimal discounting
+};
+var agent = new RL.DQNAgent(env, spec);
+
+var app = Elm.Gridworld.fullscreen();
+
+app.ports.agentAct.subscribe(function(args) {
+  // TODO use destructuring
+  var field = args[0];
+  var alreadyRewarded = args[1];
+  var state = field.concat([alreadyRewarded]);
+  var action = agent.act(state);
+  app.ports.agentMoveBot.send(action);
+});
+
+// app.ports.agentLearn.subscribe(agent.learn);
+app.ports.agentLearn.subscribe(function(reward) {
+  agent.learn(reward);
+  var x = agent.toJSON();
+  // nh: num hidden units
+  // na: max num actions
+  // ns: num states
+  // app.ports.agentLearned.send(x.net);
+});
+
+// app.ports.isMagnitude.subscribe(function(num) {
+//   return Number.isInteger(Math.log10(num));
+// });
